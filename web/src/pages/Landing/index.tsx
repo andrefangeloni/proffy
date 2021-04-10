e8c9e2e93620627630edc4f97420561463dec6db
André Angeloni <andrefangeloni@gmail.com>
2021-04-10 15:19:04 -0300

    feat: routes

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
 import studyIcon from '../../assets/images/icons/study.svg';
@@ -23,20 +25,21 @@ const Landing = () => (
       />
 
       <div className="buttons-container">
-        <a href="" className="study">
+        <Link to="/study" className="study">
           <img src={studyIcon} alt="Estudar" />
           Estudar
-        </a>
+        </Link>
 
-        <a href="" className="give-classes">
+        <Link to="/give-classes" className="give-classes">
           <img src={giveClassesIcon} alt="Dar Aulas" />
           Dar Aulas
-        </a>
+        </Link>
       </div>
 
       <span className="total-connections">
-        Total de 200 conexões já realizadas{' '}
-        <img src={purpleHeartIcon} alt="Coração Roxo"></img>
+        Total de 200 conexões já realizadas
+        {' '}
+        <img src={purpleHeartIcon} alt="Coração Roxo" />
       </span>
     </div>
   </div>
