1c7404a33df194930cd2bac7b83fda2afb7532bc
André Angeloni <andrefangeloni@gmail.com>
2021-04-12 09:23:13 -0300

    feat: start styled teacher form

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -6,7 +6,37 @@ import './styles.css';
 
 const TeacherForm = () => (
   <div id="page-teacher-form" className="container">
-    <PageHeader title="Que incrível que você quer dar aulas" />
+    <PageHeader
+      title="Que incrível que você quer dar aulas"
+      description="O primeiro passo é preencher este formulário de inscrição"
+    />
+
+    <main>
+      <fieldset>
+        <legend>Seus dados</legend>
+
+        <div className="input-block">
+          <label htmlFor="name">
+            Nome completo
+            <input type="text" id="name" />
+          </label>
+        </div>
+
+        <div className="input-block">
+          <label htmlFor="avatar">
+            Avatar
+            <input type="text" id="avatar" />
+          </label>
+        </div>
+
+        <div className="input-block">
+          <label htmlFor="whatsapp">
+            WhatsApp
+            <input type="text" id="whatsapp" />
+          </label>
+        </div>
+      </fieldset>
+    </main>
   </div>
 );
 
