f09a5fddb02beefeece41875d59b0c959f0f40a1
André Angeloni <andrefangeloni@gmail.com>
2021-04-15 22:02:39 -0300

    feat: added and styled new components

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import Input from '../../components/Input';
+import Select from '../../components/Select';
+import Textarea from '../../components/Textarea';
 import PageHeader from '../../components/PageHeader';
 
 import warningIcon from '../../assets/images/icons/warning.svg';
@@ -21,13 +23,56 @@ const TeacherForm = () => (
         <Input name="name" label="Nome completo" />
         <Input name="avatar" label="Avatar" />
         <Input name="whatsapp" label="WhatsApp" />
+        <Textarea name="bio" label="Biografia" />
       </fieldset>
 
       <fieldset>
         <legend>Sobre a aula</legend>
 
-        <Input name="subject" label="Matéria" />
+        <Select
+          name="subject"
+          label="Matéria"
+          options={[
+            { value: 'Artes', label: 'Artes' },
+            { value: 'Biologia', label: 'Biologia' },
+            { value: 'Ciências', label: 'Ciências' },
+            { value: 'Educação Física', label: 'Educação Física' },
+            { value: 'Física', label: 'Física' },
+            { value: 'Geografica', label: 'Geografica' },
+            { value: 'História', label: 'História' },
+            { value: 'Matemática', label: 'Matemática' },
+            { value: 'Português', label: 'Português' },
+            { value: 'Química', label: 'Química' },
+          ]}
+        />
+
         <Input name="cost" label="Custo da sua hora por aula" />
+      </fieldset>
+
+      <fieldset>
+        <legend>
+          Horário disponíveis
+          <button type="button">+ Novo horário</button>
+        </legend>
+
+        <div className="schedule-item">
+          <Select
+            name="week_day"
+            label="Dia da semana"
+            options={[
+              { value: '0', label: 'Domingo' },
+              { value: '1', label: 'Segunda-feira' },
+              { value: '2', label: 'Terça-feira' },
+              { value: '3', label: 'Quarta-feira' },
+              { value: '4', label: 'Quinta-feira' },
+              { value: '5', label: 'Sexta-feira' },
+              { value: '6', label: 'Sábado' },
+            ]}
+          />
+
+          <Input name="from" label="Das" type="time" />
+          <Input name="to" label="Até" type="time" />
+        </div>
       </fieldset>
 
       <footer>
@@ -39,9 +84,7 @@ const TeacherForm = () => (
           Preencha todos os dados
         </p>
 
-        <button type="button">
-          Salvar cadastro
-        </button>
+        <button type="button">Salvar cadastro</button>
       </footer>
     </main>
   </div>
